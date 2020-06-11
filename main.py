56a43be511b20f9d84bfa18f43fe7ba92ed3500b
edencreeper <teol@btinternet.com>
2020-06-11 18:59:06 +0000

    added core infrastructure

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix = '//')
+
+#Once bot online print in terminal
+@client.event
+async def on_ready():
+  print('Bot Online')
+
+
+client.run('NzIwNzExMzAyMDE1ODc3MTUx.XuJ9ow.t3PMpCSBpazWNFbvJ9FVPc89qdo')
